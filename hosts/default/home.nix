c4bf01a595480c2dec7cc5772a91f5634821e24c
Dominic Davies <dominictdavies@gmail.com>
2024-10-27 23:16:46 +0800

    Revert "Add waydroid"
    This reverts commit 71cdbef2dbc0b9f5f31075633f199bf8c199c95f.

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -79,9 +79,6 @@
     # Custom Cursors
     posy-cursors
 
-    # Android Emulator
-    waydroid
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
