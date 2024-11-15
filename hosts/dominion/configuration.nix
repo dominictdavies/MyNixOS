c537e63c64d3cb24d22e8f85c42a5d27d563f485
Dominic Davies <dominictdavies@gmail.com>
2024-11-16 00:49:36 +0800

    Make myself an op

--- hosts/dominion/configuration.nix
+++ hosts/dominion/configuration.nix
@@ -119,7 +119,6 @@
           spawn-protection = 0;
           white-list = true;
           enforce-whitelist = true;
-          enable-rcon = true;
           enforce-secure-profile = false;
         };
 
@@ -131,6 +130,10 @@
           ethan = "98bf0aab-a174-4afe-bd90-8cb5dbbae867";
           emeraldorekiller = "91694e45-1090-46fb-b83b-db40db0d3f48";
           adopolus100 = "63899fa9-9e5a-4a33-9a72-7510d0a79d1a";
+        };
+
+        operators = {
+          doomimic = "d61aa5e5-2697-4b02-bc0d-164176c9169e";
         };
       };
     };
