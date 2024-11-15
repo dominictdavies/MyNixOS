9dcaf00c6b9953f6839660bc20340614c993e449
Dominic Davies <dominictdavies@gmail.com>
2024-11-15 19:04:41 +0800

    Add nix-minecraft and rearrange lines

--- flake.nix
+++ flake.nix
@@ -3,15 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixos-hardware = {
-      url = "github:NixOS/nixos-hardware";
-    };
+    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -20,8 +19,8 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/domino/configuration.nix
-          inputs.home-manager.nixosModules.default
           inputs.nixos-hardware.nixosModules.framework-13-7040-amd
+          inputs.home-manager.nixosModules.default
         ];
       };
 
@@ -29,8 +28,8 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/dominion/configuration.nix
-          inputs.home-manager.nixosModules.default
           inputs.nixos-hardware.nixosModules.acer-aspire-4810t
+          inputs.home-manager.nixosModules.default
         ];
       };
     };
