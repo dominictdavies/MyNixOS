eb91607d157f4db745ea694d3c7fbf40012aea22
Dominic Davies <dominictdavies@gmail.com>
2024-11-15 19:11:39 +0800

    Fix syntax

--- hosts/dominion/configuration.nix
+++ hosts/dominion/configuration.nix
@@ -105,7 +105,7 @@
   services.minecraft-servers = {
     enable = true;
     eula = true;
-  }
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
