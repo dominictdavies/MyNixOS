4eaa206987ceeeae8f210144132a5101e4f60db2
Dominic Davies <dominictdavies@gmail.com>
2024-11-21 23:42:47 +0800

    Revert "Add fwupd to dominion"
    This reverts commit 3aa1b25fdf7226657da3cde6237da12f113ab4c1.

--- hosts/dominion/home.nix
+++ hosts/dominion/home.nix
@@ -22,8 +22,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    fwupd
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
