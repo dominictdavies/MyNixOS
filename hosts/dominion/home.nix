ef5dc78508d4bf95410c5e1166421ce5511de0d1
Dominic Davies <dominictdavies@gmail.com>
2025-01-24 16:06:30 +0800

    Remove networking packages

--- hosts/dominion/home.nix
+++ hosts/dominion/home.nix
@@ -34,9 +34,6 @@
     # (writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
-
-    cni-plugins
-    netplan
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
