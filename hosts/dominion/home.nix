dd68563c8e88b9df4a2ba9e5b919bae20bc24e6c
Dominic Davies <dominictdavies@gmail.com>
2025-01-28 16:48:25 +0800

    Add steamcmd to dominion

--- hosts/dominion/home.nix
+++ hosts/dominion/home.nix
@@ -22,18 +22,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    steamcmd
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
