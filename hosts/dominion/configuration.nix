1d01c891ef4f848376d860fb880bbc7b3011ab36
Dominic Davies <dominictdavies@gmail.com>
2025-02-16 01:50:15 +0800

    Add encryption mode

--- hosts/dominion/configuration.nix
+++ hosts/dominion/configuration.nix
@@ -267,6 +267,7 @@
     servers = {
       startAt = "daily";
       compression = "auto,zstd";
+      encryption.mode = "none";
       paths = "/srv/minecraft";
       environment.BORG_RSH = "ssh -i /home/dominictdavies/.ssh/id_ed25519";
       repo = "ssh://ajzc3ma4@ajzc3ma4.repo.borgbase.com/./repo";
