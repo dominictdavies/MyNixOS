11c9af246ad3f0b06a59eb8b0bfea54f0fb2a5e7
Dominic Davies <dominictdavies@gmail.com>
2025-02-16 02:32:44 +0800

    Remove directory again

--- hosts/dominion/configuration.nix
+++ hosts/dominion/configuration.nix
@@ -270,7 +270,7 @@
       encryption.mode = "none";
       paths = "/srv/minecraft";
       environment.BORG_RSH = "ssh -i /home/dominictdavies/.ssh/id_ed25519";
-      repo = "ssh://ajzc3ma4@ajzc3ma4.repo.borgbase.com/./repo";
+      repo = "ssh://ajzc3ma4@ajzc3ma4.repo.borgbase.com";
     };
   };
 
