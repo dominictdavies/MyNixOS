8a8b79e3dab4db683cf2466caaea3271be3d9149
Dominic Davies <dominictdavies@gmail.com>
2025-02-16 14:12:46 +0800

    Add valheim server backup

--- hosts/dominion/configuration.nix
+++ hosts/dominion/configuration.nix
@@ -268,7 +268,7 @@
       startAt = "daily";
       compression = "auto,zstd";
       encryption.mode = "none";
-      paths = "/srv/minecraft";
+      paths = [ "/srv/minecraft" "/var/lib/valheim/.config/unity3d/IronGate/Valheim" ];
       environment.BORG_RSH = "ssh -i /home/dominictdavies/.ssh/id_ed25519";
       repo = "ssh://ajzc3ma4@ajzc3ma4.repo.borgbase.com/./repo";
     };
