2ef18e021aecfc5c55c082c75b6d1efef7046bb1
Dominic Davies <dominictdavies@gmail.com>
2025-03-12 12:01:00 +0800

    Attempt to fix random GPU crashes

--- hosts/domino/configuration.nix
+++ hosts/domino/configuration.nix
@@ -18,6 +18,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Attempt to fix random GPU crashes
+  boot.kernelParams = [ "amdgpu.runpm=1" ];
+
   networking.hostName = "domino"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
