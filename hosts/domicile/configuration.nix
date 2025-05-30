74e6ebe7fa6a1f07730c1d105b9feb83b4a852eb
Dominic Davies <dominictdavies@gmail.com>
2025-05-30 16:25:49 +0800

    Add domicile configuration

---
+++ hosts/domicile/configuration.nix
@@ -0,0 +1,122 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+    ];
+
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.networkmanager.enable = true;
+  networking.hostName = "domicile";
+  networking.defaultGateway = "10.1.1.1";
+  networking.nameservers = [ "8.8.8.8" ];
+
+  # Static IP
+  networking.interfaces.eth0.ipv4.addresses = [ {
+    address = "10.1.1.200";
+    prefixLength = 24;
+  } ];
+
+  # Set your time zone.
+  time.timeZone = "Australia/Perth";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dominictdavies = {
+    isNormalUser = true;
+    description = "Dominic Davies";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDfj/1jtBKFkTm8pvw9uIK70KxZqTIf+cTDBXgJdiyYJjSFcWuXIznFs63CdGCPBp5nDJHE2ka92I8eIzHC8k5/kv7pU4yRZW/3IIDai1AiD56vA1vDxBf75OeHIqrYs3WBT1IJvVl57+yerPKdE11rWaKqK+M59yfTzzzmBWT75j3wtw5oabuljw+NxVgVx4oH+xj0plshShj+TrQ7FaAAFIsxQK89FRIQU8v5sTh1BIY5jcFUWOv6S+lurqL//lVF4F3ulMn0Lp7rELP4/uZsMgm4EsZmWrdXzEx4HfCiyFdWIgNqkdVZQJaPqdvWF/DsHuh0spryC+zdLjILR8rov3tgJwvxLLruhraw0xZnIvJTZWi70SC9xjn0TVU2aWblQNmj92SIJ8AeXPJESwu/rr+F1PbVLdnZ+JI3tbTW7N8/mlMSMO5F0P4/9GmE6FtOUsBbX3BydXFPRDavc4vimQusdmfBEm2553sKISLWV3uS7KBf6lHxxqn9NupVIz8= dominictdavies@domino"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDYWWLuj8iaTVE/2TQbXhosNCzQcRB+RhMvnPHcXfgxrH4XqpF0we5xF1tC94zFDzzn3VwYkQpL2kKSdtSVNCyGfywPIOZpSQc5zuiNfc50uajN+XBkECYdVgdiJLRPOwCIECyTllSyJUmA+KKjjNzKOGLCmBCkVhZ6XiJ1+ErxUzYy5PXYZip3N2OiIgS3nACtqpHe2Jo9bvP6kZi9QxpWa87C0M7zW65uHKDYusJxKlYe2fKBF6jfHniMN0ZuQvWaoZ3y4lGp5mlQf2QtUdNgm9197LpHsSWVAidbxVTa+2yDNpJv33rnOcAmhpSrfOrGNzOaqmObkCde8HCTsopg9YFT3ixmDRjMltd/+YfY+j/3BT331lRIBD1uDysQYb6+84LVdq9HnEQ6DzX69C2ZmX1koE9pCTq84KGzK6WReE6rf4AK0JXmnE23dTRV4GDnNIPW/y4nl+8XWRS6kusvVtACd6/shU48YjQke5Yxuf2JDMvUb2LMPUZQvRo4zgMd5Vmen9eutK599GrPbZsKh7Rm9jnSR+xHr7s7umYbPmTSe14t5Ka73uf80nj0SFRersKDxzZbPlIN9eUy8g6SajTXzGKiWdOBMP5hSCLpyOd26toWl3NW7hUrPrg4XTJbxxNKG33glYkrS6SAZF7mXrr3PLYavk/iwxPyVs8NOQ== dominictdavies@dominator"
+    ];
+  };
+
+  home-manager = {
+    # Also pass inputs to home-manager modules
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "dominictdavies" = import ./home.nix;
+    };
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Allow nh to find flake directory
+  environment.sessionVariables = {
+    NH_FLAKE = "/home/dominictdavies/NixOS";
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # Modify the EFI Boot Manager
+    efibootmgr
+
+    # Yet another nix cli helper
+    nh
+
+    # Distributed version control system
+    git
+
+    # GitHub CLI tool
+    gh
+  ];
+
+  # Prevent laptop from sleeping on lid close
+  services.logind = {
+    lidSwitch = "ignore";
+    lidSwitchDocked = "ignore";
+    lidSwitchExternalPower = "ignore";
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = false;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 443 ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
