dba52e46541e45120d15dbb38bfed511180e976e
Dominic Davies <dominictdavies@gmail.com>
2025-06-02 22:40:55 +0800

    Use static IP again

--- hosts/domicile/configuration.nix
+++ hosts/domicile/configuration.nix
@@ -25,10 +25,10 @@
   networking.nameservers = [ "8.8.8.8" ];
 
   # Static IP
-  # networking.interfaces.eth0.ipv4.addresses = [ {
-  #   address = "192.168.0.200";
-  #   prefixLength = 24;
-  # } ];
+  networking.interfaces.eth0.ipv4.addresses = [ {
+    address = "10.1.1.200";
+    prefixLength = 24;
+  } ];
 
   # Set your time zone.
   time.timeZone = "Australia/Perth";
