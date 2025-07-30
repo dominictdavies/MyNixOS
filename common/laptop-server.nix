a5ec909dbd5217bf6fc4ba12da6d21382382d268
Dominic Davies <dominictdavies@gmail.com>
2025-07-30 18:04:32 +0800

    Declare openssh

--- common/laptop-server.nix
+++ common/laptop-server.nix
@@ -6,8 +6,10 @@
   networking.nameservers = [ "8.8.8.8" ];
 
   # Use authorised keys rather than password
-  services.openssh.enable = true;
-  services.openssh.settings.PasswordAuthentication = false;
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+  };
 
   # Prevent laptop from sleeping on lid close
   services.logind = {
