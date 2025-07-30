74bce22343ba19de0878c8a573f3723d7048e770
Dominic Davies <dominictdavies@gmail.com>
2025-07-30 17:58:18 +0800

    Prevent clobbering

--- common/environment.nix
+++ common/environment.nix
@@ -1,19 +1,17 @@
 { pkgs, ... }:
 
 {
-  environment = {
-    systemPackages = with pkgs; [
-      # NixOS CLI Tool
-      nh
+  environment.systemPackages = with pkgs; [
+    # NixOS CLI Tool
+    nh
 
-      # Git & GitHub
-      git
-      gh
-    ];
+    # Git & GitHub
+    git
+    gh
+  ];
 
-    sessionVariables = {
-      # Allow nh to find flake directory
-      NH_FLAKE = "/home/dominictdavies/MyNixOS";
-    };
+  environment.sessionVariables = {
+    # Allow nh to find flake directory
+    NH_FLAKE = "/home/dominictdavies/MyNixOS";
   };
 }
--- hosts/dominion/grafana-and-prometheus.nix
+++ hosts/dominion/grafana-and-prometheus.nix
@@ -1,44 +1,42 @@
 { config, ... }:
 
 {
-  services = {
-    grafana = {
+  services.grafana = {
+    enable = true;
+    settings.server.http_addr = "0.0.0.0";
+    settings.server.http_port = 3000;
+    provision = {
       enable = true;
-      settings.server.http_addr = "0.0.0.0";
-      settings.server.http_port = 3000;
-      provision = {
-        enable = true;
-        datasources.settings.datasources = [
-          {
-            name = "Prometheus";
-            type = "prometheus";
-            access = "proxy";
-            url = "http://localhost:${toString config.services.prometheus.port}";
-          }
-        ];
-      }; 
-    };
-
-    prometheus = {
-      enable = true;
-      listenAddress = "localhost";
-      port = 9090;
-
-      exporters.node = {
-        enable = true;
-        listenAddress = "localhost";
-        port = 9100;
-      };
-
-      scrapeConfigs = [
+      datasources.settings.datasources = [
         {
-          job_name = "Node";
-          scrape_interval = "10s";
-          static_configs = [
-            { targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ]; }
-          ];
+          name = "Prometheus";
+          type = "prometheus";
+          access = "proxy";
+          url = "http://localhost:${toString config.services.prometheus.port}";
         }
       ];
+    }; 
+  };
+
+  services.prometheus = {
+    enable = true;
+    listenAddress = "localhost";
+    port = 9090;
+
+    exporters.node = {
+      enable = true;
+      listenAddress = "localhost";
+      port = 9100;
     };
+
+    scrapeConfigs = [
+      {
+        job_name = "Node";
+        scrape_interval = "10s";
+        static_configs = [
+          { targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ]; }
+        ];
+      }
+    ];
   };
 }
