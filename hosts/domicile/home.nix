e22bd0add62cff35bc0672dc024b0dcdc9d6835f
Dominic Davies <dominictdavies@gmail.com>
2025-07-31 18:51:49 +0800

    Remove home comments

--- hosts/domicile/home.nix
+++ hosts/domicile/home.nix
@@ -1,40 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "dominictdavies";
   home.homeDirectory = "/home/dominictdavies";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  # Make sure to check the Home Manager release notes if you want to change this value
+  home.stateVersion = "24.05";
 
   # Allow unfree and broken packages
   nixpkgs.config.allowUnfree = true;
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
   programs = {
-    # Bash
     bash = {
       enable = true;
 
