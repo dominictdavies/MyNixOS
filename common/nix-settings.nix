3ecf6caa77d93b672df7fe05c5f4cecb771b2c55
Dominic Davies <dominictdavies@gmail.com>
2025-09-13 16:01:42 +0800

    Add trusted users

--- common/nix-settings.nix
+++ common/nix-settings.nix
@@ -1,6 +1,7 @@
 { ... }:
 
 {
+  nix.settings.trusted-users = [ "root" "dominictdavies" ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
 }
