cde8dd08d6bdece6ea365a17c3d6637b02864050
Dominic Davies <dominictdavies@gmail.com>
2025-10-24 15:41:41 +0800

    Switch to SDL3

--- hosts/domino/home.nix
+++ hosts/domino/home.nix
@@ -79,7 +79,7 @@
         cpprun() {
           local name="$1"
           shift
-          g++ "''${name}.cpp" -o "''${name}.out" -lSDL2 &&
+          g++ "''${name}.cpp" -o "''${name}.out" -lSDL3 &&
           ./"''${name}.out" "$@"
         }
       '';
