82ce9bf5986914e1e40c76074281ea7a55d865fa
Dominic Davies <dominictdavies@gmail.com>
2025-12-15 16:52:29 +0800

    Bash the run

--- hosts/dominion/services.nix
+++ hosts/dominion/services.nix
@@ -9,7 +9,7 @@
       Type = "exec";
       User = "dominictdavies";
       WorkingDirectory = "/home/dominictdavies/NeoForge";
-      ExecStart = "/home/dominictdavies/NeoForge/run.sh";
+      ExecStart = "/bin/bash /home/dominictdavies/NeoForge/run.sh";
       Restart = "on-failure";
       TimeoutStopSec=60;
     };
