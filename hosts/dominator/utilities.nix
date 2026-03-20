ba8a663a1d91780867fcc181aa99d78f82a18f9c
dominictdavies <dominictdavies@gmail.com>
2026-03-20 22:34:28 +0800

    Add logitech wireless

--- hosts/dominator/utilities.nix
+++ hosts/dominator/utilities.nix
@@ -3,6 +3,12 @@
 {
   hardware.graphics.enable = true;
   hardware.bluetooth.enable = true;
+
+  hardware.logitech.wireless = {
+    enable = true;
+    enableGraphical = true;
+  };
+
   services.printing.enable = true;
   services.fwupd.enable = true;
 }
