0beb8869750a61c89184342de488e4e23c7598f9
Dominic Davies <dominictdavies@gmail.com>
2026-03-20 14:44:32 +0800

    Add garbage collection

--- common/nix-settings.nix
+++ common/nix-settings.nix
@@ -4,4 +4,11 @@
   nix.settings.trusted-users = [ "root" "dominictdavies" ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
+
+  # Garbage collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 }
