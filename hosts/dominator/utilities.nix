c99fe8abd0e5c7b265e95189b9cbf6d3a8e5e5d1
dominictdavies <dominictdavies@gmail.com>
2026-03-21 00:37:50 +0800

    Revert "Add logitech wireless"
    This reverts commit ba8a663a1d91780867fcc181aa99d78f82a18f9c.

--- hosts/dominator/utilities.nix
+++ hosts/dominator/utilities.nix
@@ -3,12 +3,6 @@
 {
   hardware.graphics.enable = true;
   hardware.bluetooth.enable = true;
-
-  hardware.logitech.wireless = {
-    enable = true;
-    enableGraphical = true;
-  };
-
   services.printing.enable = true;
   services.fwupd.enable = true;
 }
