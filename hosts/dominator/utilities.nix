a5edc56fdb83cb514adf5534c91a8fe2a3514a7b
dominictdavies <dominictdavies@gmail.com>
2026-03-21 00:37:44 +0800

    Revert "Enable ratbagd"
    This reverts commit 01f3c1c19aafc2d652018760d2a64c69d1f958b5.

--- hosts/dominator/utilities.nix
+++ hosts/dominator/utilities.nix
@@ -3,6 +3,7 @@
 {
   hardware.graphics.enable = true;
   hardware.bluetooth.enable = true;
-  services.printing.enable = true;
   services.fwupd.enable = true;
+  services.ratbagd.enable = true;
+  services.printing.enable = true;
 }
