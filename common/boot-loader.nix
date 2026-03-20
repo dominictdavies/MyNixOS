c3c9fc2a898764b98f1a9ed576344da9fc9a3981
Dominic Davies <dominictdavies@gmail.com>
2026-03-20 15:11:15 +0800

    Limit boot configurations to 16

--- common/boot-loader.nix
+++ common/boot-loader.nix
@@ -1,6 +1,10 @@
 { ... }:
 
 {
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 16;
+  };
+
   boot.loader.efi.canTouchEfiVariables = true;
 }
