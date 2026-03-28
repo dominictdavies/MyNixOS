17467e98c8d07a7326bac33b41b69fb719354eaf
dominictdavies <dominictdavies@gmail.com>
2026-03-28 14:37:07 +0800

    Empty configuration

--- modules/hosts/dominator/utilities.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-
-{
-  hardware.graphics.enable = true;
-  hardware.bluetooth.enable = true;
-  services.fwupd.enable = true;
-  services.ratbagd.enable = true;
-  services.printing.enable = true;
-}
--- modules/hosts/dominion/scripts/backup-neoforge-server.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-BACKUP_FILE="/home/dominictdavies/Backups/world-ours/world-ours_$(date +%Y-%m-%d).tar.gz"
-
-systemctl stop neoforge-server
-tar -czf "$BACKUP_FILE" -C /home/dominictdavies/NeoForge world-ours/
-chown dominictdavies:users "$BACKUP_FILE"
-systemctl reboot
