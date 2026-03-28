2a2e1a3b97446e9d6328885934f57cefb32c24aa
dominictdavies <dominictdavies@gmail.com>
2026-03-28 18:33:31 +0800

    Add noctalia

--- modules/wrappedPrograms/niri.nix
+++ modules/wrappedPrograms/niri.nix
@@ -13,12 +13,17 @@
     {
       pkgs,
       lib,
+      self',
       ...
     }:
     {
       packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
         inherit pkgs;
         settings = {
+          spawn-at-startup = [
+            (lib.getExe self'.packages.myNoctalia)
+          ];
+
           xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
 
           input.keyboard.xkb.layout = "us,ua";
@@ -28,6 +33,7 @@
           binds = {
             "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
             "Mod+Q".close-window = null;
+            "Mod+S".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
           };
         };
       };
