62c75e27e9a7c1f0c0be47aa07c8af01c0cf11b3
dominictdavies <dominictdavies@gmail.com>
2026-03-28 17:13:35 +0800

    Add users

---
+++ modules/nixos/common/users.nix
@@ -0,0 +1,15 @@
+{ self, inputs, ... }:
+{
+  flake.nixosModules.common =
+    { ... }:
+    {
+      users.users.dominictdavies = {
+        isNormalUser = true;
+        description = "Dominic Davies";
+        extraGroups = [
+          "wheel"
+          "networkmanager"
+        ];
+      };
+    };
+}
