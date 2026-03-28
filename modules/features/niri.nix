202038dcbe3ae6969af4be1d19979aa693bcfeb0
dominictdavies <dominictdavies@gmail.com>
2026-03-28 15:59:53 +0800

    Make niri a module

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -1,5 +1,14 @@
 { self, inputs, ... }:
 {
+  flake.nixosModules.niri =
+    { pkgs, lib, ... }:
+    {
+      programs.niri = {
+        enable = true;
+        package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
+      };
+    };
+
   perSystem =
     {
       pkgs,
