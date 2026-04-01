c238a7bf6d1120db0f23489dabdd36ad3053c2c4
Dominic Davies <dominictdavies@gmail.com>
2026-04-02 02:07:02 +0800

    Zero second bootloader timeout

--- modules/nixos/common/boot.nix
+++ modules/nixos/common/boot.nix
@@ -1,13 +1,12 @@
 {
   flake.nixosModules.common = {
     boot.loader = {
+      timeout = 0;
       efi.canTouchEfiVariables = true;
       systemd-boot = {
         enable = true;
         configurationLimit = 10;
       };
-
-      timeout = 3;
     };
   };
 }
