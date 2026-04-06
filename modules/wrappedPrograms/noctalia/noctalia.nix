c602cf04011a96b0a0837e3a497b9b26dd55ffc1
dominictdavies <dominictdavies@gmail.com>
2026-04-06 23:57:33 +0800

    Fix `mynix-noctalia-push`

--- modules/wrappedPrograms/noctalia/noctalia.nix
+++ modules/wrappedPrograms/noctalia/noctalia.nix
@@ -8,7 +8,7 @@
       ];
 
       programs.bash.shellAliases = {
-        mynix-noctalia-push = "(cd ~/MyNixOS/modules/wrappedPrograms/noctalia && nix run nixpkgs#noctalia-shell ipc call state all > noctalia.json && git add noctalia.json && git commit -m \"Update noctalia settings\" && git push)";
+        mynix-noctalia-push = "(cd ~/MyNixOS/modules/wrappedPrograms/noctalia && nix run nixpkgs#noctalia-shell ipc call state all > config.json && git add config.json && git commit -m \"Update noctalia settings\" && git push)";
       };
     };
 
