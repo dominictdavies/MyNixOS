b7a01aeade3dcf0e3121ec761312e6f491c97ad3
dominictdavies <dominictdavies@gmail.com>
2026-04-27 17:26:41 +0800

    Continuous prompting for conversation

--- scripts/gemma.py
+++ scripts/gemma.py
@@ -14,23 +14,34 @@ model = AutoModelForCausalLM.from_pretrained(
 
 # Prompt
 messages = [
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Write a short joke about saving RAM."},
+    {"role": "system", "content": "You are Gemma, a helpful assistant that answers all messages concisely."},
+    {"role": "user", "content": "Hello! Introduce yourself please."},
 ]
 
-# Process input
-text = processor.apply_chat_template(
-    messages, 
-    tokenize=False, 
-    add_generation_prompt=True, 
-    enable_thinking=False
-)
-inputs = processor(text=text, return_tensors="pt").to(model.device)
-input_len = inputs["input_ids"].shape[-1]
+if __name__ == "__main__":
+    while True:
+        try:
+            # Process input
+            text = processor.apply_chat_template(
+                messages, 
+                tokenize=False, 
+                add_generation_prompt=True, 
+                enable_thinking=False
+            )
+            inputs = processor(text=text, return_tensors="pt").to(model.device)
+            input_len = inputs["input_ids"].shape[-1]
 
-# Generate output
-outputs = model.generate(**inputs, max_new_tokens=1024)
-response = processor.decode(outputs[0][input_len:], skip_special_tokens=False)
+            # Generate output
+            outputs = model.generate(**inputs, max_new_tokens=1024)
+            response = processor.decode(outputs[0][input_len:], skip_special_tokens=True)
 
-# Parse thinking
-processor.parse_response(response)
+            # Parse thinking
+            processor.parse_response(response)
+            print(f"Gemma: {response}")
+
+            # Next message
+            messages.append({"role": "user", "content": input("Message: ")})
+
+        except KeyboardInterrupt:
+            print("Goodbye...")
+            break
