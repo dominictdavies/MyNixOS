0dbb319adefe7d619aa6071ca05b48b444b53301
dominictdavies <dominictdavies@gmail.com>
2026-04-28 18:37:14 +0800

    "init" to "shell"

--- modules/nixos/common/bash.nix
+++ modules/nixos/common/bash.nix
@@ -19,7 +19,7 @@
           mynix-all-switch = "mynix-pull && mynix-update && mynix-switch";
 
           # Shells
-          init_python = "cp ~/MyNixOS/shells/python.nix ./shell.nix && echo use nix > .envrc && direnv allow .";
+          shell_python = "cp ~/MyNixOS/shells/python.nix ./shell.nix && echo use nix > .envrc && direnv allow .";
 
           # General
           list = "ls -lA";
