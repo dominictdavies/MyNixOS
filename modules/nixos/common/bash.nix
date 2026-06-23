fd7fdf76dc5104752fe2130f71d4d948317ba932
dominictdavies <dominictdavies@gmail.com>
2026-06-24 00:02:12 +0800

    Add more boot nix commands

--- modules/nixos/common/bash.nix
+++ modules/nixos/common/bash.nix
@@ -11,13 +11,16 @@
 
         shellAliases = {
           # MyNixOS
-          mynix-pull = "(cd ~/MyNixOS && git pull --rebase)";
-          mynix-boot = "nh os boot";
           mynix-switch = "nh os switch";
+          mynix-boot = "nh os boot";
+          mynix-pull = "(cd ~/MyNixOS && git pull --rebase)";
           mynix-pull-switch = "mynix-pull && mynix-switch";
+          mynix-pull-boot = "mynix-pull && mynix-boot";
           mynix-update = "(cd ~/MyNixOS && git pull --rebase && nix flake update && git add flake.lock && git commit -m \"Update flake\" && git push)";
           mynix-update-switch = "mynix-update && mynix-switch";
+          mynix-update-boot = "mynix-update && mynix-boot";
           mynix-all-switch = "mynix-pull && mynix-update && mynix-switch";
+          mynix-all-boot = "mynix-pull && mynix-update && mynix-boot";
 
           # Shells
           shell = "cp ~/MyNixOS/shells/shell.nix ./shell.nix && echo use nix > .envrc && direnv allow .";
