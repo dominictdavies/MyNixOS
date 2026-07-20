84cf70a0d64bc64f93a512faef8b33d85e842a84
dominictdavies <dominictdavies@gmail.com>
2026-07-20 15:00:53 +0800

    Disable fingerprint auth

--- modules/features/desktop.nix
+++ modules/features/desktop.nix
@@ -50,6 +50,11 @@
         printing.enable = true;
       };
 
+      security = {
+        polkit.enable = true;
+        pam.services.login.fprintAuth = false;
+      };
+
       programs = {
         seahorse.enable = true;
 
