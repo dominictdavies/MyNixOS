ff9c1c4ee21e4322dd2592f373980d84ea6b917e
dominictdavies <dominictdavies@gmail.com>
2026-07-26 20:03:32 +0800

    Update shell.nix version

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   # Check for new commits at https://status.nixos.org
   pkgs =
     import
-      (fetchTarball "https://github.com/NixOS/nixpkgs/archive/01fbdeef22b76df85ea168fbfe1bfd9e63681b30.tar.gz")
+      (fetchTarball "https://github.com/NixOS/nixpkgs/archive/335f0738cb2fa9708f3f428e39d2eae975d1338d.tar.gz")
       { };
 in
 pkgs.mkShell {
