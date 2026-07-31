e0532b2e92f45c47be2132a8d203302f2d99246f
dominictdavies <dominictdavies@gmail.com>
2026-08-01 01:43:16 +0800

    Revert "Permit insecure electron"
    This reverts commit e4de0394e7030a45b79d9008a647546a5094519d.

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -11,11 +11,6 @@
       options = "--delete-older-than 30d";
     };
 
-    nixpkgs.config = {
-      allowUnfree = true;
-      permittedInsecurePackages = [
-        "electron-39.8.10"
-      ];
-    };
+    nixpkgs.config.allowUnfree = true;
   };
 }
