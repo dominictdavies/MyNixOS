5a11985ac2550ca370e07fdbb3909abaf20a8a92
dominictdavies <dominictdavies@gmail.com>
2026-08-01 01:52:57 +0800

    Declare unfree packages

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,16 +1,31 @@
 {
-  flake.nixosModules.common = {
-    nix.settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+  flake.nixosModules.common =
+    { lib, ... }:
+    {
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
 
-    nix.gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
+      nix.gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+      };
+
+      nixpkgs.config.allowUnfreePredicate =
+        pkg:
+        builtins.elem (lib.getName pkg) [
+          # Firmware
+          "b43-firmware"
+          "broadcom-bt-firmware"
+          "facetimehd-firmware"
+          "facetimehd-calibration"
+          "xone-dongle-firmware"
+
+          # Steam
+          "steam"
+          "steam-unwrapped"
+        ];
     };
-
-    nixpkgs.config.allowUnfree = true;
-  };
 }
