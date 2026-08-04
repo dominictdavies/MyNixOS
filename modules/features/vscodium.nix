cc99b50bbc0cd5ede0dd674797daa4d300962ec7
dominictdavies <dominictdavies@gmail.com>
2026-08-04 21:47:12 +0800

    Link vscodium keybindings

--- modules/features/vscodium.nix
+++ modules/features/vscodium.nix
@@ -8,7 +8,8 @@
       ];
 
       systemd.user.tmpfiles.rules = [
-        "L+ %h/.config/VSCodium/User/settings.json - - - - ${config.my.repoRoot}/dotfiles/vscodium.json"
+        "L+ %h/.config/VSCodium/User/settings.json - - - - ${config.my.repoRoot}/dotfiles/vscodium/settings.json"
+        "L+ %h/.config/VSCodium/User/keybindings.json - - - - ${config.my.repoRoot}/dotfiles/vscodium/keybindings.json"
       ];
     };
 }
