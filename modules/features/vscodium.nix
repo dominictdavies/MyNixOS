ffeca480696458a083473472d3372f3c5fe554ca
dominictdavies <dominictdavies@gmail.com>
2026-08-26 20:13:47 +0800

    Declare extensions

--- modules/features/vscodium.nix
+++ modules/features/vscodium.nix
@@ -2,9 +2,39 @@
   flake.nixosModules.vscodium =
     { pkgs, config, ... }:
     {
-      environment.systemPackages = with pkgs; [
-        vscodium
-      ];
+      programs.vscode = {
+        enable = true;
+        package = pkgs.vscodium;
+        extensions =
+          with pkgs.vscode-extensions;
+          [
+            aaron-bond.better-comments
+            davidanson.vscode-markdownlint
+            james-yu.latex-workshop
+            jnoortheen.nix-ide
+            tamasfe.even-better-toml
+          ]
+          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+              publisher = "github";
+              name = "vscode-pull-request-github";
+              version = "0.156.0";
+              sha256 = "sha256-Go9yiqCPZoJkHgHAaH42mawnZOCVlqMp5I+NG68RrPE=";
+            }
+            {
+              publisher = "phil294";
+              name = "git-log--graph";
+              version = "0.1.35";
+              sha256 = "sha256-xDR+3zBlkgPBQihGUFvTrtt8yH8gbcBg6pFKGtOEBHI=";
+            }
+            {
+              publisher = "jeandeaual";
+              name = "lilypond-syntax";
+              version = "0.1.1";
+              sha256 = "sha256-Lo4Opa9PaMlCxLRx+6n6r2f/El2+N0gEMAO6cd9l7Fo=";
+            }
+          ];
+      };
 
       systemd.user.tmpfiles.rules = [
         "L+ %h/.config/VSCodium/User/settings.json - - - - ${config.my.repoRoot}/dotfiles/vscodium/settings.json"
