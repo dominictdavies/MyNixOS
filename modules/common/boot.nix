72839ec46b86f44fdea26c377c5d4dd8f060396e
dominictdavies <dominictdavies@gmail.com>
2026-09-05 05:55:03 +0800

    Revert "Disable USB autosuspend"
    This reverts commit dd16f6c61c56db8cd505f7541648425da9293366.

--- modules/common/boot.nix
+++ modules/common/boot.nix
@@ -1,19 +1,12 @@
 {
   flake.nixosModules.common = {
-    boot = {
-      loader = {
-        timeout = 3;
-        efi.canTouchEfiVariables = true;
-        limine = {
-          enable = true;
-          maxGenerations = 10;
-        };
+    boot.loader = {
+      timeout = 3;
+      efi.canTouchEfiVariables = true;
+      limine = {
+        enable = true;
+        maxGenerations = 10;
       };
-
-      # Disable USB autosuspend
-      extraModprobeConfig = ''
-        options usbcore autosuspend=-1
-      '';
     };
   };
 }
