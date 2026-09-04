db3e795fb6d14e430db25c1901134d0bbbe9c2a4
dominictdavies <dominictdavies@gmail.com>
2026-09-05 06:39:19 +0800

    Add correct zfs options

--- modules/features/zfs.nix
+++ modules/features/zfs.nix
@@ -6,6 +6,9 @@
         zfs
       ];
 
+      networking.hostId = "01234567";
+      boot.supportedFilesystems = [ "zfs" ];
+      boot.zfs.forceImportRoot = false;
       services.zfs.autoScrub.enable = true;
     };
 }
